2094c05ef319c5e39207750f3ae5db7a6aef901d
unknown <charles.a7cantara@gmail.com>
2026-02-28 18:24:49 +0800

    fixed runtime error for horizontal timeline component

--- data/education.ts
+++ data/education.ts
@@ -4,55 +4,63 @@ export const education = [
     school: "Dumantay Elementary School",
     seal: "/pictures/De_La_Salle_University_Seal.svg",
     year: "2006",
-    degree: "Nursery"
+    degree: "Nursery",
+    description: ""
   },
   {
     id: 2,
     school: "University of Batangas",
     seal: "/pictures/De_La_Salle_University_Seal.svg",
     year: "2007 - 2009",
-    degree: "Preschool"
+    degree: "Preschool",
+    description: ""
   },
   {
     id: 3,
     school: "Saint Bridget College",
     seal: "/pictures/De_La_Salle_University_Seal.svg",
     year: "2009 - 2010",
-    degree: "Grade 1"
+    degree: "Grade 1",
+    description: ""
   },
   {
     id: 4,
     school: "Saint Therese Multiple Intelligence School",
     seal: "/pictures/De_La_Salle_University_Seal.svg",
     year: "2010 - 2011",
-    degree: "Grade 2"
+    degree: "Grade 2",
+    description: ""
   },
   {
     id: 5,
     school: "Dukhan English School",
     seal: "/pictures/De_La_Salle_University_Seal.svg",
     year: "2011 - 2014",
-    degree: "Grade 3 - 6"
+    degree: "Grade 3 - 6",
+    description: ""
   },
   {
     id: 6,
     school: "McKinley Hill School",
     seal: "/pictures/De_La_Salle_University_Seal.svg",
     year: "2014 - 2019",
-    degree: "Grade 7 - 10"
+    degree: "Grade 7 - 10",
+    description: ""
   },
   {
     id: 7,
     school: "Lyceum of the Philippines University",
     seal: "/pictures/De_La_Salle_University_Seal.svg",
     year: "2019 - 2021",
-    degree: "Senior High"
+    degree: "Senior High",
+    description: ""
   },
   {
     id: 8,
     school: "De La Salle University - Manila",
     seal: "/pictures/De_La_Salle_University_Seal.svg",
     year: "2021 - 2025",
-    degree: "Bachelor of Science in Software Technology"
+    degree: "Bachelor of Science in Software Technology",
+    description: ""
   },
 ];
